c4347355d83f199e66456c69df18b81371718238
maonguyen8x <nguyenmao.2912@gmail.com>
2022-09-23 22:10:39 +0700

    Add users

--- index.js
+++ index.js
@@ -11,18 +11,23 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URL,
-        { useNewUrlParser: true, useUnifiedTopology: true },
+        {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+        },
         () => {
-        console.log("Connected to MongoDB !");
-});
+                console.log("Connected to MongoDB !");
+        }
+);
 
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req, res) => {
         res.send("Welcome to homepage")
--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,21 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // Register
-router.get("/register", async(req, res) => {
-        const user = await new User({
+router.get("/register", async (req, res) => {
+        try {
+                const newUser = await new User({
                 username: "Marvin",
                 email: "nguyenmao.2912@gmail.com",
                 password: "123456",
         });
 
-        await user.save();
-        res.send("ok");
+        const user = await newUser.save();
+        res.status(200).json(user);
+        } catch (err) {
+                console.log(err);
+                res.status(500).json(err);
+        }
+        
 });
 
 module.exports = router;
