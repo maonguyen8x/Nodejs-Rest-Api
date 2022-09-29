dc431ec795a05c698a0de0600363d6316a0312c2
BE-HT_MaoNV <maonv@hybrid-technologies.vn>
2022-09-29 17:07:04 +0700

    Change path of files

---
+++ api/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const options = {
+	autoIndex: false, // Don't build indexes
+	maxPoolSize: 10, // Maintain up to 10 socket connections
+	serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+	family: 4 // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect(process.env.MONGO_URL, options,
+	() => {
+		console.log("Connected to MongoDB !");
+	}
+);
+
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/post", postRoute);
+
+app.get("/", (req, res) => {
+	res.send("Welcome to homepage")
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}...`)
+});
+
+
---
+++ api/routes/users.js
@@ -0,0 +1,102 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// Update user
+router.put("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		if (req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(10);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			} catch (e) {
+				return res.status(500).json({Message: e.message})
+			}
+		}
+		try {
+			const user = await User.findOneAndUpdate(req.params.id, {
+				$set: req.body,
+			});
+			res.status(200).json({user, Message: "Account has been updated!"})
+		} catch (e) {
+			return res.status(500).json({Message: e.message})
+		}
+
+	} else {
+		return res.status(403).json({Message: "You can update only your account."})
+	}
+});
+
+// Delete user
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			await User.findByIdAndDelete(req.params.id);
+			res.status(200).json({Message: "Account has been deleted!"});
+		} catch (e) {
+			return res.status(500).json({Message: e.message});
+		}
+
+	} else {
+		return res.status(403).json({Message: "You can delete only your account."});
+	}
+});
+
+// Get user
+router.get("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findById(req.params.id);
+			res.status(200).json(user);
+		} catch (err) {
+			res.status(500).json({Message: err.message});
+		}
+	}
+});
+
+// Follow user
+router.put("/:id/follow", async (req, res) => {
+	if (req.body.userId !== req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+
+			if (!user.followers.includes(req.body.userId)) {
+				await user.updateOne({$push: {followers: req.body.userId}});
+				await currentUser.updateOne({$push: {followings: req.params.id}});
+				res.status(200).json({Message: "User has been followed."});
+			} else {
+				res.status(403).json({Message: "You already follow this user."});
+			}
+		} catch (err) {
+			res.status(500).json({Message: err.message});
+		}
+	} else {
+		res.status(403).json({Message: "You cannot follow yourself."});
+	}
+});
+
+// Unfollow user
+router.put("/:id/unfollow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+
+			if (user.followers.includes(req.body.userId)) {
+				await user.updateOne({$pull: {followers: req.body.userId}});
+				await currentUser.updateOne({$pull: {followings: req.params.id}});
+
+				res.status(200).json({Message: "User has been unfollowed."});
+			} else {
+				res.status(403).json({Message: "You do not follow this user."});
+			}
+		} catch (err) {
+			res.status(500).json({Message: err.message});
+		}
+	} else {
+		res.status(403).json({Message: "You cannot unfollow yourself."});
+	}
+});
+
+module.exports = router;
