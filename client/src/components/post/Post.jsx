21cb22e9145785dc124e181967563c90b79c8155
maonguyen8x <nguyenmao.2912@gmail.com>
2022-09-29 22:14:45 +0700

    Edit path img

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -37,8 +37,8 @@ export default function Post({post}) {
 				</div>
 				<div className="postBottom">
 					<div className="postBottomLeft">
-						<img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt=""/>
-						<img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt=""/>
+						<img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt=""/>
+						<img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt=""/>
 						<span className="postLikeCounter">{like} people like it</span>
 					</div>
 					<div className="postBottomRight">
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,11 +1,14 @@
 import "./topbar.css";
 import {Chat, Notifications, Person, Search} from "@material-ui/icons";
+import {Link} from "react-router-dom";
 
 export default function Topbar() {
 	return (
 		<div className="topbarContainer">
 			<div className="topbarLeft">
-				<span className="logo">Facebook</span>
+				<Link to="/" style={{textDecoration: "none"}}>
+					<span className="logo">Facebook</span>
+				</Link>
 			</div>
 			<div className="topbarCenter">
 				<div className="searchbar">
